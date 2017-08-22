01e8b3d937978a24db4c401efe70e1048efc9d6e
Alexandr Li <alexli0612@gmail.com>
2017-08-22 16:58:30 +1200

    Set Location

--- src/nz/ac/unitec/main/Main.java
+++ src/nz/ac/unitec/main/Main.java
@@ -1,12 +1,15 @@
 package nz.ac.unitec.main;
 
 import java.awt.Dimension;
+import java.awt.Toolkit;
 
 public class Main {
 	public static void main(String args[]) {
         GameFrame frame = new GameFrame();
         frame.setMinimumSize(new Dimension(800, 600));
         frame.setMinimumSize(new Dimension(400, 400));
+        frame.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - frame.getWidth())/2, 
+                (Toolkit.getDefaultToolkit().getScreenSize().height - frame.getHeight())/2);
         frame.setVisible(true);
 	}
 }
